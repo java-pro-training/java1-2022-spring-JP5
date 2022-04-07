05f5241d0b2ed1ac085c84acd1610c2960c7da32
diana stafecka <dianiite@gmail.com>
2022-04-07 19:57:43 +0300

    Mana pirmā klase :)

--- stafecka-diana/src/main/java/lv/lu/training/Hello_world_DST.java
+++ stafecka-diana/src/main/java/lv/lu/training/Hello_world_DST.java
@@ -2,6 +2,6 @@ package lv.lu.training;
 
 public class Hello_world_DST {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
