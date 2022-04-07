f19fde396a96848f993b62a62a541afab46a5262
Tanchiks <tanja.roscina@inbox.lv>
2022-04-07 21:55:56 +0300

    manā pirmā pankūka

--- roscina-tatjana/src/main/java/lv/lu/training/HelloWord.java
+++ roscina-tatjana/src/main/java/lv/lu/training/HelloWord.java
@@ -1,7 +1,11 @@
 package lv.lu.training;
 
+import java.util.Random;
+
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Random generator=new Random();
+        int randomNumber=generator.nextInt(101);
+        System.out.println(randomNumber);
     }
 }
