6b3ac80458720ba8d9039f154669d732452f4ebc
Kristine Mironova <kristine.mironova@inbox.lv>
2022-04-10 23:04:12 +0300

    Mana pirma klase

--- mironova-kristine/src/main/java/lv/lu/training/pirmaisMajasDarbs_1.java
+++ mironova-kristine/src/main/java/lv/lu/training/pirmaisMajasDarbs_1.java
@@ -1,10 +1,11 @@
 package lv.lu.training;
 
-public class pirmaisMajasDarbs {
+public class pirmaisMajasDarbs_1 {
     public static void main(String[] args) {
         String part1 = "Hello";
         String part2 = "World";
         String result = part1 + " " + part2;
         System.out.println(result);
+
 }
 }
