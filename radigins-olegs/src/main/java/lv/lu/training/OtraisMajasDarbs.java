5344259526578b706cfa077b82a59c3993360104
olegsradigin <oradigins@mail.com>
2022-04-10 12:55:40 +0300

    OtraisMajasDarbs

--- radigins-olegs/src/main/java/lv/lu/training/OtraisMajasDarbs.java
+++ radigins-olegs/src/main/java/lv/lu/training/OtraisMajasDarbs.java
@@ -1,6 +1,6 @@
 package lv.lu.training;
 
-public class OtraisMajasDarbs {
+public class MajasDarbs {
     public static void main(String[] args) {
         String part1 = "Hello";
         String part2 = "World";
