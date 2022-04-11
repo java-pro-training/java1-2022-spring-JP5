f17021e6109948e55e00994cfd7a2f5f40d57db7
ozolsedgars <ozols.edgars@outlook.com>
2022-04-11 22:33:39 +0300

    Update settings.gradle

---
+++ ozols-edgars/src/main/java/lv/lu/training/TringleAreaCalculator.java
@@ -0,0 +1,12 @@
+package lv.lu.training;
+
+public class TringleAreaCalculator {
+    public static void main(String[] args) {
+        int base = 10;
+        int heigt = 20;
+
+        int result = (base * heigt) /2;
+
+        System.out.print(result);
+    }
+}
---
+++ ozols-edgars/src/main/java/lv/lu/training/majasdarbs2v2.java
@@ -0,0 +1,17 @@
+package lv.lu.training;
+
+public class majasdarbs2v2 {
+    public static void main(String[] args) {
+        int side1 = 10;
+        int side2 = 15;
+        int side3 = 14;
+        int side4 = 25;
+
+        int result = side1; // perimetrs
+        result = result + side2;
+        result += side3;
+        result += side4;
+
+        System.out.print("perimetrs: " + result);
+    }
+}
