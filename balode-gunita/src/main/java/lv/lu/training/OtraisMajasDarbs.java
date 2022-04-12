0ade7740b8063ef2761a43f57154a90b3bbf2c16
G-unit-a <gunita.balode@gmail.com>
2022-04-13 01:01:25 +0300

    2. Mainigie un operatori - aritmetika, kvadrata perimetrs

--- balode-gunita/src/main/java/lv/lu/training/OtraisMajasDarbs.java
+++ balode-gunita/src/main/java/lv/lu/training/OtraisMajasDarbs.java
@@ -2,10 +2,14 @@ package lv.lu.training;
 
 public class OtraisMajasDarbs {
     public static void main(String[] args) {
-        String part1 = "Hello";
-        String part2 = "World";
-        String result = part1 + " " + part2;
-        System.out.println(result);
-
+        int side1 = 10;
+        int side2 = 15;
+        int side3 = 14;
+        int side4 = 25;
+        int result = side1;
+        result = result + side2;
+        result += side3;
+        result += side4;
+        System.out.println("perimetrs: " + result);
     }
 }
