7e187c111c3c4717a87cb94ed4d458febf4d92cf
M1dzis <edgars.spiridovskis@gmail.com>
2022-04-14 17:24:19 +0300

    1_majas_darbs

---
+++ spiridovskis-edgars/src/main/java/lv/lu/training/majas_darbs1.java
@@ -0,0 +1,11 @@
+package lv.lu.training;
+
+
+public class majas_darbs1 {
+    public static void main(String[] args) {
+        String part1 = "Hello";
+        String part2 = "World";
+        String result = part1 + " " + part2;
+        System.out.println(result);
+    }
+}
