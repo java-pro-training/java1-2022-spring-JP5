0cf7814ab9902924d75522f0bae374e32de9b9de
Reinis <reinis.balers@gmail.com>
2022-04-14 18:02:38 +0300

    OtraisMajasdarbs

--- balers-reinis/src/main/java/lv/lu/training/TriangleAreaCalculator.java
+++ balers-reinis/src/main/java/lv/lu/training/TriangleAreaCalculator.java
@@ -2,10 +2,12 @@ package lv.lu.training;
 
 public class TriangleAreaCalculator {
     public static void main(String[] args) {
-        int base = 10;
-        int height = 20;
+        int base = 13;
+        int height = 7;
         int result = (base * height)/2;
-        System.out.println("perimetrs: " + result);
+
+        System.out.println("result = " + result);
+
 
     }
 }
