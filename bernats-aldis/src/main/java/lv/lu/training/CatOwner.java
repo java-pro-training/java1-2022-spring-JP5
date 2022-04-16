6330af6094ab6678bac6f5fbe5b6fb33e399b755
Keksis <aldis.valmiera@gmail.com>
2022-04-16 14:12:52 +0300

    CatOwner

--- bernats-aldis/src/main/java/lv/lu/training/CatOwner.java
+++ bernats-aldis/src/main/java/lv/lu/training/CatOwner.java
@@ -2,7 +2,7 @@ package lv.lu.training;
 
 public class CatOwner {
     public static void main(String[] args) {
-        Cat myCat = new Cat("black", "toms", 7);
+        Cat myCat = new Cat("gray", "boss", 7);
         System.out.println("My cat's color - " + myCat.getColor());
         System.out.println("My cat's weight - " + myCat.getWeight());
         System.out.println("My cat's name - " + myCat.getName());
