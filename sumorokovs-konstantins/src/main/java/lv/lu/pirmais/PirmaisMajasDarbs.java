534c62ef1f8dbb45b000d32c101a420131ef2fd4
K0stco <konsumor94@gmail.com>
2022-04-21 17:04:51 +0300

    3 punkts no 1 majasdarba

---
+++ sumorokovs-konstantins/src/main/java/lv/lu/pirmais/PirmaisMajasDarbs.java
@@ -0,0 +1,11 @@
+package lv.lu.pirmais;
+
+
+public class PirmaisMajasDarbs {
+    public static void main(String[] args) {
+    int pirmaisMainigais = 10;
+    int otraisMainigais =20;
+    int tresaisMainigais = pirmaisMainigais + otraisMainigais;
+        System.out.println("tresaisMainigais");
+    }
+}
