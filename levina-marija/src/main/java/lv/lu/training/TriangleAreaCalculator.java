2b2ef472abc8d5031af65896bfe798f4790d32c3
Marija Levina <marija.levina@inbox.lv>
2022-04-21 18:09:05 +0300

    Mana pirma klase

---
+++ levina-marija/src/main/java/lv/lu/training/TriangleAreaCalculator.java
@@ -0,0 +1,12 @@
+package lv.lu.training;
+
+import java.io.PrintStream;
+
+public class TriangleAreaCalculator {
+    public static void main(String[] args) {
+    int base = 8;
+    int height = 2;
+    int result = base * height;
+        System.out.println(result);
+    }
+}
