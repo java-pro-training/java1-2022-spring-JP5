cc10dcc26307264ff1d7d2efbd3c1d66560e0140
Kristine Mironova <kristine.mironova@inbox.lv>
2022-04-21 22:55:49 +0300

    Mana pirma klase

---
+++ mironova-kristine/src/main/java/lv/lu/training/homework1/Hello.java
@@ -0,0 +1,13 @@
+package lv.lu.training.homework1;
+import java.util.Random;
+public class Hello {
+    public static void main(String[] args) {
+        int pirmaisMainigais = 10;
+        int otraisMainigas = 20;
+        int tresaisMainigais = pirmaisMainigais + otraisMainigas;
+                System.out.println(tresaisMainigais);
+
+    }
+
+}
+
