66716e8a05c03459e6d9b938fd99c57449f4c67e
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-04-23 18:45:29 +0300

    MajasDarbs3

--- alexeeva-viktorija/src/main/java/lv/lu/training/Helloworld.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/Helloworld.java
@@ -7,8 +7,8 @@ public class Helloworld {
 public static void main(String[] args) {
     int pirmaisMainigais = 10;
     int otraisMainigais = 20;
-    int trešaisMainigais = pirmaisMainigais + otraisMainigais;
-    System.out.println(trešaisMainigais);
+    int tresaisMainigais = pirmaisMainigais + otraisMainigais;
+    System.out.println(tresaisMainigais);
 
     Random randomGenerator = new Random();
     int randomNumber = randomGenerator.nextInt(101);
