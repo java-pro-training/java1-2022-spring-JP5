b406d8870cc0ae7018db2e1604ee36020e28d69e
K0stco <konsumor94@gmail.com>
2022-04-25 10:39:19 +0300

    2 punkts no 1 majasdarba

---
+++ sumorokovs-konstantins/src/main/java/lv/lu/pirmais/Cat.java
@@ -0,0 +1,42 @@
+package lv.lu.pirmais;
+
+public class Cat {
+private String color = "Black";
+private String name = "Aza";
+private String species = "Null";
+private int age = 4;
+private int weight = 120;
+
+    public Cat(String color, String name, int weight) {
+        this.color = color;
+        this.name = name;
+        this.weight = weight;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public String getName() {
+        return name;
+    }
+   public void setName(String name){
+        this.name = name;
+    }
+    public String getSpecies(){
+        return species;
+    }
+    public void setSpecies(){
+        this.species = Null;
+    }
+    public String getAge(String age){
+
+    public int getAge("4") {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
