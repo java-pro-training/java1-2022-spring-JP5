7c9e2c8d35a6e443eac0df0555e7d1eee9b5be1f
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-04-28 22:33:09 +0300

    MajasDarbs5

---
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs5/CardUser.java
@@ -0,0 +1,17 @@
+package lv.lu.training.majasdarbs5;
+
+import java.time.LocalDate;
+
+public class CardUser {
+    public static void main(String[] args) {
+        BankCard myCard = new BankCard("Banka Citadele", "Visa", "Roberts Polis", "2345678345", LocalDate.of(2024,12,10), "134");
+        System.out.println("Mana karte: " + myCard);
+
+        myCard.addFunds(100d);
+        System.out.println("Mana karte: " + myCard);
+
+        myCard.withdraw(20d);
+        System.out.println("Mana karte: " + myCard);
+
+            }
+}
