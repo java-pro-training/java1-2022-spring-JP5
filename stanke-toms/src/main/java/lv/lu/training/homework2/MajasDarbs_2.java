ed1794d139df86dd08e39e8b864aa2a37dd08106
tomss <toms.stanke@gmail.com>
2022-04-28 08:41:08 +0300

    Pirmais

---
+++ stanke-toms/src/main/java/lv/lu/training/homework2/MajasDarbs_2.java
@@ -0,0 +1,13 @@
+package lv.lu.training.homework2;
+
+public class MajasDarbs_2 {
+    public static void main(String[] args) {
+        String part1 = "Hello";
+        String part2 = "World";
+        String result = part1 + " " + part2;
+        System.out.println(result);
+
+
+
+    }
+}
