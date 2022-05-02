ad531dd24296ce9f79c6e04ee68d58091cbf406b
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-05-02 21:59:49 +0300

    MajasDarbs5

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs5/Operations.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs5/Operations.java
@@ -5,6 +5,7 @@ public class Operations {
     public static void main(String[] args) {
         BankAccount myAccount = new BankAccount("987654321", "1234", 100, 200, 50);
 
+
         myAccount.topUp(30, "1234");
         System.out.println("Current Balance is " + myAccount);
 
