7ae0c5a1c4c25d133d9be69f177263bdb54c40df
ozolsedgars <ozols.edgars@outlook.com>
2022-05-04 23:36:30 +0300

    cet

---
+++ ozols-edgars/src/main/java/lv/lu/training/homework4/rollDice.java
@@ -0,0 +1,29 @@
+package lv.lu.training.homework4;
+
+import javax.xml.transform.Result;
+import java.util.Random;
+
+public class rollDice {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int dice1 = random.nextInt(6)+1;
+
+        int dice2 = random.nextInt(6)+1;
+
+         int Result = dice1+dice2;
+
+
+         if (Result == 7 || Result ==11){
+            System.out.println("You win!");
+         }else{
+            System.out.println("You loose!");
+        }
+
+
+
+
+
+    }
+}
