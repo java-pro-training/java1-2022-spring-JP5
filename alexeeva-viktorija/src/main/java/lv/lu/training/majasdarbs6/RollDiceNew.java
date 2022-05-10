86b6fcb0d3b94eb2961bb3332c156ca0c21ef534
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-05-10 20:53:37 +0300

    MajasDarbs6

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs6/RollDiceNew.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs6/RollDiceNew.java
@@ -4,29 +4,34 @@ import java.util.Random;
 
 public class RollDiceNew {
     public static void main(String[] args) {
-        Random random = new Random ();
-
-        int dice1 = random.nextInt( 1,  7 );
-        int dice2 = random.nextInt(1,7);
-
-        int result = dice1 + dice2;
+        Random random = new Random();
 
         int sumOfWins = 0;
         int sumOfLoses = 0;
+        int result = 0;
+        int dice1;
+        int dice2;
 
         while (result < 12) {
+            dice1 = random.nextInt(1, 7);
+            dice2 = random.nextInt(1, 7);
+
+            result = dice1 + dice2;
 
             if (result == 7 || result == 11) {
-                sumOfWins ++;
+                sumOfWins++;
                 System.out.println("You win " + sumOfWins + " times");
 
             } else if (result == 2 || result == 3 || result == 6) {
-                sumOfLoses ++;
+                sumOfLoses++;
                 System.out.println("You lose " + sumOfLoses + " times");
-
+            } else if (result == 12) {
+                System.out.println("Game over!");
             } else {
-                System.out.println("Try again");
+                System.out.println("try again");
             }
+
         }
+
     }
 }
