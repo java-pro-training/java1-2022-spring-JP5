b3a79cbe1ac2e9bfd722bc0dacc25d5607e2482f
Oskars Linde <oskars.linde.external@kneip.com>
2022-05-12 21:56:09 +0300

    7 nodarbība

--- teacher/src/main/java/lv/lu/training/seventhlecture/HowManyTimes.java
+++ teacher/src/main/java/lv/lu/training/seventhlecture/HowManyTimes.java
@@ -11,11 +11,13 @@ public class HowManyTimes {
     public static void main(String[] args) {
         //Izveidot programmu, kas atrod cik reizes konkrētais vārds atkārtojas masīvā.
         String[] words = {"John", "Wick", "Nora", "Bumbiere", "John", "Vitautas", "Zigurds", "John", "Nora"};
-        String wordToSearch = "Viktors";
+        String wordToSearch = "John";
         int repeatCount = 0;
-        //Jūsu Kods
-        // for(...
-        // ...
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(wordToSearch)){
+                repeatCount++;
+            }
+        }
         System.out.println("Word [" + wordToSearch + "] is repeated [" + repeatCount + "] times");
     }
 }
