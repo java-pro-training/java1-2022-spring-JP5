07ae97743eb2056730acbd3fd6fc2e2d6ae77969
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-05-18 17:27:45 +0300

    MajasDarbs7

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs7/ArrayUtilsTest.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs7/ArrayUtilsTest.java
@@ -3,9 +3,9 @@ package lv.lu.training.majasdarbs7;
 public class ArrayUtilsTest {
     public static void main(String[] args) {
 
-        int[] result = ArrayUtils.reverse(new int[]{1, 2, 3});
+        int[] result = ArrayUtils.reverse(new int[]{1, 5, 9});
         System.out.print("Test of method 'reverse', works .. ");
-        if (result[0] == 3 && result[1] == 2 && result[2] == 1) {
+        if (result[0] == 9 && result[1] == 5 && result[2] == 1) {
             System.out.println("Correctly");
         } else {
             System.out.println("Incorrectly");
