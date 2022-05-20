2f5605402bc75369da66ee6c1c35f864cb1a7a6e
AnnaMalahova <a.zitare@inbox.lv>
2022-05-20 12:32:28 +0300

    6tais majasdarbs_1.dala

--- malahova-anna/src/main/java/lv/lu/training/RollDiceSecond.java
+++ malahova-anna/src/main/java/lv/lu/training/RollDiceSecond.java
@@ -6,34 +6,49 @@ public class RollDiceSecond {
 
     public static void main(String[] args) {
 
+        //izveido objektu
         Random random = new Random();
-        int dice1 = random.nextInt(6) + 1;
-        int dice2 = random.nextInt(6) + 1;
-        int diceSum = dice1 + dice2;
 
-
+        //deklare mainigos,define izejas punktus (...=0;)
+        int dice1;
+        int dice2;
+        int diceSum = 0;
         int sumOfWins = 0;
         int sumOfLoss = 0;
 
-        while(diceSum != 12){
-          if (diceSum == 7 || diceSum == 11){
-              sumOfWins++;
-              System.out.println("Sum of Wins is: " + sumOfWins);
-          }
-        }
+        while (diceSum < 12) {
 
-        while(diceSum != 12) {
-            if (diceSum == 2 || diceSum == 3 || diceSum == 6){
+            dice1 = random.nextInt(6) + 1;
+            dice2 = random.nextInt(6) + 1;
+            diceSum = dice1 + dice2;
+
+            if (diceSum == 7 || diceSum == 11) {
+                sumOfWins++;
+                System.out.println("Sum of Wins: " + sumOfWins);
+
+            } else if (diceSum == 2 || diceSum == 3 || diceSum == 6) {
                 sumOfLoss++;
-                System.out.println("Sum of Loss is: " + sumOfLoss);
+                System.out.println("Sum of Loss: " + sumOfLoss);
+            } else if (diceSum == 12) {
+                System.out.println("You lose!");
+            } else {
+                System.out.println("Try again!");
             }
-        }
-
-
 
         }
-
-
-
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
