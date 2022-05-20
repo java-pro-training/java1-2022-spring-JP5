363532fda1b250a1ef2271e0b5f63a7e944e889e
AnnaMalahova <a.zitare@inbox.lv>
2022-05-20 11:52:25 +0300

    5tais majasdarbs_izlabots

--- malahova-anna/src/main/java/lv/lu/training/BankAccount.java
+++ malahova-anna/src/main/java/lv/lu/training/BankAccount.java
@@ -1,54 +1,78 @@
 package lv.lu.training;
 
-import java.util.Scanner;
-
 public class BankAccount {
-    private String countNumber;
+
+    //define mainigos
+    private String accountNumber;
     private String pinCode;
     private double debetBalance;
     private double creditLimit;
     private double usedCredit;
+    private double amount;
 
-    public BankAccount(String countNumber, String pinCode, double debetBalance, double creditLimit, double usedCredit) {
-        this.countNumber = countNumber;
+    //konstruktors
+    public BankAccount(String accountNumber, String pinCode, double debetBalance, double creditLimit, double usedCredit) {
+        this.accountNumber = accountNumber;
         this.pinCode = pinCode;
         this.debetBalance = debetBalance;
         this.creditLimit = creditLimit;
         this.usedCredit = usedCredit;
     }
 
-public boolean topUp(double amount, String pinCode){
-    double moneyForBalance = creditLimit - usedCredit;
+    //naudas iznemsanas metode
+    public void withdraw(double amount, String pinCode) {
 
-    if (pinCode.equals(userPinCode)) {
+        double availableAmount = debetBalance + (creditLimit - usedCredit);
+        double isEnoughFunds = availableAmount - amount;
 
-        this.usedCredit += insertMoney;
-
-    } else {
-        System.out.println("Wrong pin code");
+        if (pinCode == this.pinCode) {
+            System.out.println("PIN is correct!");
+            if (isEnoughFunds > 0.00) {
+                System.out.println("Funds are available in the account!");
+                if (debetBalance >= amount) {
+                    debetBalance -= amount;
+                } else if (availableAmount >= amount) {
+                    usedCredit += amount - debetBalance;
+                    debetBalance = 0;
+                } else  {
+                    System.out.println("Not enough money!");
+                }
+            } else {
+                System.out.println("Funds are not available in the account!");
+            }
+        } else {
+            System.out.println("PIN is incorrect!");
+        }
     }
 
-}
-
-public boolean withDraw(String pinCode, double amount){
-    double availableAmount = debetBalance + (creditLimit - usedCredit);
-    boolean isEnoughtMoney = availableAmount - amount > 0.001;
-
-    if (pinCode.equals(userPincode)) {
-        this.debetBalance -= amount;
-    } else {
-        System.out.print("Wrong pin code");
-
+    //GB: Naudas iemaksas metode
+    public void topup(double amount, String pinCode) {
+        if (pinCode == this.pinCode) {
+            System.out.println("PIN is correct!");
+            if (usedCredit > 0.00 && amount >= usedCredit) {
+                debetBalance += amount - usedCredit;
+                usedCredit = 0;
+            } else if (usedCredit > 0.00 && amount <= usedCredit) {
+                usedCredit -= amount;
+            } else {
+                debetBalance += amount;
+            }
+        } else {
+            System.out.println("PIN is incorrect! Try again!");
+        }
     }
 
-
-}
+    //GB: Lai izdrukatu objekta MoneyTransaction laukus
     @Override
     public String toString() {
-        return ", debetBalance=" + debetBalance +
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", pinCode='" + pinCode + '\'' +
+                ", debitBalance=" + debetBalance +
                 ", creditLimit=" + creditLimit +
-                ", usedCredit=" + usedCredit;
+                ", usedCreditLimit=" + usedCredit +
+                ", amount=" + amount +
+                '}';
     }
-
 
 }
--- malahova-anna/src/main/java/lv/lu/training/RollDiceSecond.java
+++ malahova-anna/src/main/java/lv/lu/training/RollDiceSecond.java
@@ -7,18 +7,33 @@ public class RollDiceSecond {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int dice1 = random.nextInt(6)+1;
-        int dice2 = random.nextInt(6)+1;
-        int result = dice1 + dice2;
+        int dice1 = random.nextInt(6) + 1;
+        int dice2 = random.nextInt(6) + 1;
+        int diceSum = dice1 + dice2;
 
-        if (result == 7) {
-            System.out.println("You win!!!");
-        } else if (result == 11) {
-            System.out.println("You win!!!");
-        } else {
-            System.out.println("You loss!");
+
+        int sumOfWins = 0;
+        int sumOfLoss = 0;
+
+        while(diceSum != 12){
+          if (diceSum == 7 || diceSum == 11){
+              sumOfWins++;
+              System.out.println("Sum of Wins is: " + sumOfWins);
+          }
+        }
+
+        while(diceSum != 12) {
+            if (diceSum == 2 || diceSum == 3 || diceSum == 6){
+                sumOfLoss++;
+                System.out.println("Sum of Loss is: " + sumOfLoss);
+            }
         }
 
 
-    }
+
+        }
+
+
+
+
 }
