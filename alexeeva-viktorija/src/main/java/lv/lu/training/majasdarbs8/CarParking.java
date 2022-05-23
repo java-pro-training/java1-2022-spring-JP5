d35bd7e43e0f0d4853f4f548766d996f1ed054bd
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-05-23 20:14:45 +0300

    MajasDarbs8

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/CarParking.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/CarParking.java
@@ -1,5 +1,8 @@
 package lv.lu.training.majasdarbs8;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarParking {
     public static void main(String[] args) {
         Buss myNewBuss = new Buss("Red", 2020, "Volvo", 50);
@@ -9,5 +12,13 @@ public class CarParking {
         System.out.println("My old buss " + myOldBuss);
         System.out.println("Are busses equals: " + myNewBuss.equals(myOldBuss));
 
+        Truck myTruck = new Truck("black", 2010, "Man", 3000);
+        Car myCar = new Car("Green", 2021, "BMW");
+
+        List<Car> allMyVehicles = Arrays.asList(myCar, myNewBuss, myOldBuss, myTruck);
+        for (Car car : allMyVehicles) {
+            System.out.println(car);
+
+        }
     }
 }
