2c3cd98cdb1cd2c9ea885459b9da802077a7778e
G-unit-a <gunita.balode@gmail.com>
2022-05-23 18:36:36 +0300

    6. Majas darbs - Cikla operatori 6.2. Nepara noteiksana // Odd Numbers Lookup

---
+++ balode-gunita/src/main/java/lv/lu/training/homework6/OddNumberTest.java
@@ -0,0 +1,8 @@
+package lv.lu.training.homework6;
+
+public class OddNumberTest {
+    public static void main(String[] args) {
+        OddNumberLookup oddNumberLookup = new OddNumberLookup();
+        oddNumberLookup.printOddWithin( 1, 10);
+    }
+}
