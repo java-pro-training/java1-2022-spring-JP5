45eb5d27940f32a20736ddd667597bdb289c474b
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-05-30 22:29:08 +0300

    MajasDarbs8

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/md1/Buss.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/md1/Buss.java
@@ -1,4 +1,4 @@
-package lv.lu.training.majasdarbs8.md2.md1;
+package lv.lu.training.majasdarbs8.md1;
 
 import java.util.Objects;
 
--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/md1/Passenger.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs8/md1/Passenger.java
@@ -1,4 +1,4 @@
-package lv.lu.training.majasdarbs8.md2.md1;
+package lv.lu.training.majasdarbs8.md1;
 
 public interface Passenger {
     int passengerSeatCount();
