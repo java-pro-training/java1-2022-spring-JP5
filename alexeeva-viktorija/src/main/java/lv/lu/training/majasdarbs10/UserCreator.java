06d8b64f29bccf6a5861355151c8b41bbec655dd
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-06-08 20:48:38 +0300

    MajasDarbs10

---
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs10/UserCreator.java
@@ -0,0 +1,11 @@
+package lv.lu.training.majasdarbs10;
+
+public class UserCreator {
+    public static void main(String[] args) {
+        UserValidator validator = new UserValidator();
+        validator.validate(new User("Anna","Upe", 35));
+        validator.validate(new User("John","Smith", 125));
+        validator.validate(new User("Kristaps-Peteris","Ozols", 17));
+        validator.validate(new User("Maris","Lucavsalis", 65));
+    }
+}
