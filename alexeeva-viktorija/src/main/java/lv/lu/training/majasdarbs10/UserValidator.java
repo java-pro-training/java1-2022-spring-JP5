82ae42be7bb3aeed2cad4188e8cf61b566b52202
Viktorija-Alexeeva <vika_da_90@mail.ru>
2022-06-09 07:56:23 +0300

    MajasDarbs10

--- alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs10/UserValidator.java
+++ alexeeva-viktorija/src/main/java/lv/lu/training/majasdarbs10/UserValidator.java
@@ -1,14 +1,24 @@
 package lv.lu.training.majasdarbs10;
 
 public class UserValidator {
+
     public void validate(User user) {
-        if (hasInvalidLength(user.getName()) || hasInvalidLength(user.getSurname())) {
-            throw new ValidationException("Name and surname has to be 3 to 15 characters long");
+        try {
+            if (hasInvalidLength(user.getName()) || hasInvalidLength(user.getSurname())) {
+                throw new ValidationException("Name and surname has to be 3 to 15 characters long");
+            }
+
+            if (hasInvalidAge(user.getAge())) {
+                throw new ValidationException("Age must be within human possibility : 0 - 120");
+            }
+        } catch (ValidationException exception) {
+            System.out.println("Ir noticis ValidationException");
+
+            System.out.println("Please enter correct information: ");
+        } finally {
+            System.out.println("User is added into system.");
         }
 
-        if (ageCheck(user.getAge())) {
-            throw new ValidationException("Age must be within human possibility : 0 - 120");
-        }
     }
 
     private static final int MIN_TEXT_LENGTH = 3;
@@ -20,7 +30,7 @@ public class UserValidator {
         return text.length() < MIN_TEXT_LENGTH || text.length() > MAX_TEXT_LENGTH;
     }
 
-    private boolean ageCheck(int age) {
+    private boolean hasInvalidAge(int age) {
         return age < MIN_AGE || age > MAX_AGE;
     }
 
