8415d53a8c070a8c467268f921c6754c9f5b117a
Kristine Mironova <kristine.mironova@inbox.lv>
2022-06-13 20:47:38 +0300

    majas darbi

--- mironova-kristine/src/main/java/lv/lu/training/homework9/WordCounter.java
+++ mironova-kristine/src/main/java/lv/lu/training/homework9/WordCounter.java
@@ -1,11 +1,9 @@
 package lv.lu.training.homework9;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class WordCounter {
+
     public static void main(String[] args) {
         new WordCounter().run();
     }
@@ -17,27 +15,35 @@ public class WordCounter {
                         "Matematika",
                         "Zoologija",
                         "Literatura",
-                        "Matematika"};
+                        "Anatomija"};
 
         List<String> wordList = Arrays.asList(wordArray);
-
 
         for (String word : wordList) {
             addWord(word, wordCounts);
         }
         System.out.println(wordCounts);
+
+        for(String key: sortKeys(wordCounts)){
+            System.out.println("["+key+"="+wordCounts.get(key)+"]");
+        }
+
     }
 
     public static void addWord(String word, Map<String, Integer> wordMap) {
-        if(wordMap.get(word) != null) {
-            int count = wordMap.get(word);
+        Integer count = wordMap.get(word);
+        if (count != null) {
             count++;
             wordMap.put(word, count);
         } else {
             wordMap.put(word, 1);
         }
     }
-}
-
+        List<String> sortKeys(Map<String,Integer> map){
+            List<String> listOfKeys = new ArrayList<>(map.keySet());
+            Collections.sort(listOfKeys);
+            return listOfKeys;
+        }
+    }
 
 
