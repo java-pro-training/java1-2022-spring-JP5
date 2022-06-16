8bbf6b9e53406185d434c2bcec7392591935f67b
Oskars Linde <oskars.linde.external@kneip.com>
2022-06-16 19:55:28 +0300

    Jaunais zars rulleee

--- teacher/src/main/java/lv/lu/training/lecture12/GitExample.java
+++ teacher/src/main/java/lv/lu/training/lecture12/GitExample.java
@@ -2,6 +2,6 @@ package lv.lu.training.lecture12;
 
 public class GitExample {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello JAUNAIS ZARS!");
     }
 }
